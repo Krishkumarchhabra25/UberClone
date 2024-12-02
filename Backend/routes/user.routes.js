106883dfbd5ff0a00e5089e4e643569c72463741
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2024-12-02 22:18:07 +0530

    userlogin done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,15 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {body} = require("express-validator")
 const userController = require("../controllers/user.controller")
+
 router.post("/register" ,[
     body('email').isEmail().withMessage('Inavlid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('first name must be at least 3 characters long'),
     body('password').isLength({min:6}).withMessage('password must be 6 characters long')
 ],
 userController.registerUser
-
 )
 
-
+router.post("/login",[
+    body('email').isEmail().withMessage('Inavlid Email'),
+    body('password').isLength({min:6}).withMessage('password must be 6 characters long')
+],
+userController.loginUser
+)  
 
 module.exports = router
