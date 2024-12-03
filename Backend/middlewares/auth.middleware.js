5fe778f1eff3db52e42fd81df5b2375671a081af
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2024-12-03 18:11:10 +0530

    login signup and login page is done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -2,7 +2,8 @@ const userModel = require("../models/user.model");
 const captainModel = require("../models/captain.model")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const blacklistTokenModel = require("../models/blacklistToken.model")
+const blacklistTokenModel = require("../models/blacklistToken.model");
+
 module.exports.authUser = async(req, res,next)=>{
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
 
